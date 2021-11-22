982a8e62b01ff7881bfde8385b46cdaf37cd1f30
c267huan <c267huan@student.cs.uwaterloo.ca>
2021-11-21 21:32:18 -0500

    vending machine finished

--- groupOff.h
+++ groupOff.h
@@ -3,9 +3,11 @@
 
 _Monitor Printer;
 _Task Groupoff {
+	unsigned int numStudents, sodaCost, groupoffDelay;
+	Printer & printer;
 	void main();
   public:
 	Groupoff( Printer & prt, unsigned int numStudents, unsigned int sodaCost, unsigned int groupoffDelay )
-		:numStudents(numStudents), sodaCost(sodaCost), groupoffDelay(groupoffDelay) {}
+		:numStudents(numStudents), sodaCost(sodaCost), groupoffDelay(groupoffDelay), printer(prt) {}
 	WATCard::FWATCard giftCard();
 };
