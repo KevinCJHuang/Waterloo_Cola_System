bb59a9b5e385549a616db5a64dec628d32f6b32b
lichendd <l93yu@uwaterloo.ca>
2021-11-22 14:27:07 -0500

    added printer calls on groupOff.cc and changed printer calls in parent.cc

--- groupOff.cc
+++ groupOff.cc
@@ -7,17 +7,20 @@ Groupoff::Groupoff( Printer & prt, unsigned int numStudents, unsigned int sodaCo
 	}
 
 void Groupoff::main() {
+	printer.print(Printer::Kind::Groupoff, 'S');
 	unsigned int i;
 	unsigned int studentsDone = 0;
 	for ( i = 0; i < numStudents; i++) {
 		_Accept (giftCard);
+		printer.print(Printer::Kind::Groupoff, 'D', sodaCost);
 		studentsDone++;
 	}
 
 	for ( i = 0; i < numStudents; i++) {
 		giftCards[i].delivery(new WATCard());
-	_Accept (~Groupoff) { break; } _Else
+		_Accept (~Groupoff) { break; } _Else
 	}
+	printer.print(Printer::Kind::Groupoff, 'F');
 }
 
 WATCard::FWATCard Groupoff::giftCard() {
